43f6b1d58af167b8788161f974531400694b53dc
Anjna11 <anjnajagatiya@gmail.com>
2025-10-22 19:41:37 +0530

    dynamic patient rows.

--- healthcare_next/app/home/patients/page.tsx
+++ healthcare_next/app/home/patients/page.tsx
@@ -9,24 +9,42 @@ import {
   } from "@/components/ui/table"
 
 export default function Patients() {
+
+    const patients_ls = [
+        {
+            name: "ABC",
+            age: 12
+        },
+        {
+            name: "XYZ",
+            age: 12
+        },
+        {
+            name: "XYZ",
+            age: 12
+        }
+    ]
+
     return (
         <Table>
             <TableCaption>A list of your recent invoices.</TableCaption>
             <TableHeader>
                 <TableRow>
-                <TableHead className="w-[100px]">Invoice</TableHead>
-                <TableHead>Status</TableHead>
-                <TableHead>Method</TableHead>
-                <TableHead className="text-right">Amount</TableHead>
+                <TableHead>Name</TableHead>
+                <TableHead>Age</TableHead>
                 </TableRow>
             </TableHeader>
             <TableBody>
-                <TableRow>
-                <TableCell className="font-medium">INV001</TableCell>
-                <TableCell>Paid</TableCell>
-                <TableCell>Credit Card</TableCell>
-                <TableCell className="text-right">$250.00</TableCell>
-                </TableRow>
+                {
+                    patients_ls.map((item, index) => {
+                      return (
+                        <TableRow key={index}>
+                            <TableCell>{item.name}</TableCell>
+                            <TableCell>{item.age}</TableCell>
+                        </TableRow>
+                      )  
+                    })
+                }
             </TableBody>
         </Table>
     )
